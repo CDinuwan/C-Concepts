62878a8c80c62be72ddf177efd24260b7a2ca684
CDinuwan <chanukadinuwan35@gmail.com>
2021-01-30 11:08:53 +0530

    Access modifiers

---
+++ Access Modifiers/Access Modifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.SetBirthdate(new DateTime(1999, 11, 12));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
