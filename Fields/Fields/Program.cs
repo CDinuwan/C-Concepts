2ce3253177b2e99fe0e28cd3e67e8359adb90af5
CDinuwan <chanukadinuwan35@gmail.com>
2021-01-30 11:36:41 +0530

    Fields

---
+++ Fields/Fields/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders=new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+        public Customer(int id,string name)
+            :this(id)
+        {
+            this.Name = name;
+        }
+        public void Promte()
+        {
+
+        }
+    }
+    public class Order
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+
+            customer.Promte();
+        }
+    }
+}
